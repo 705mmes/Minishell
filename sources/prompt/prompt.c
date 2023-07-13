e4b68f5af63383bae59267e8f95a1ae088182476
JERINECK Léon <ljerinec@lj.local>
2023-07-13 17:11:26 +0200

    ctrl-c OK

--- sources/prompt/prompt.c
+++ sources/prompt/prompt.c
@@ -6,7 +6,7 @@
 /*   By: ljerinec <ljerinec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 15:24:27 by ljerinec          #+#    #+#             */
-/*   Updated: 2023/07/13 16:42:53 by ljerinec         ###   ########.fr       */
+/*   Updated: 2023/07/13 17:08:33 by ljerinec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,29 +25,19 @@ void	prompt(void)
 {
 	char	*line;
 
-	line = new_prompt();
-	if (line[0] == '\n' || line[0] == '\0')
+	while (1)
 	{
-		free(line);
 		line = new_prompt();
-	}
-	else if (line)
-	{
-		write(0, "\0", 1);
-		printf(" minishell: command not found: %s", line);
 		free(line);
-		line = new_prompt();
 	}
-	else
-		free(line);
 }
 
 void	signal_changement(void)
 {
 	struct sigaction	s_sigaction;
 
-	s_sigaction.sa_sigaction = sig_control;
 	s_sigaction.sa_flags = SA_SIGINFO;
+	s_sigaction.sa_sigaction = sig_control;
 	sigaction(SIGQUIT, &s_sigaction, 0);
 	sigaction(SIGINT, &s_sigaction, 0);
 }
@@ -57,7 +47,7 @@ void	sig_control(int sig, siginfo_t *info, void *context)
 	(void) info;
 	(void) context;
 	if (sig == SIGINT)
-		write(0, "\0", 1);
+		write(0, "\0\n", 2);
 	else if (sig == SIGQUIT)
-		write(0, "\0", 1);
+		exit(1);
 }
