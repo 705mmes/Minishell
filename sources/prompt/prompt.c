635a97aedf966f587158be69ec40a1ecbe550de4
Leon Jerineck <ljerinec@k0r1p6.42mulhouse.fr>
2023-07-18 17:07:49 +0200

    Checking OK

--- sources/prompt/prompt.c
+++ sources/prompt/prompt.c
@@ -6,7 +6,7 @@
 /*   By: ljerinec <ljerinec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 12:37:12 by ljerinec          #+#    #+#             */
-/*   Updated: 2023/07/18 15:07:58 by ljerinec         ###   ########.fr       */
+/*   Updated: 2023/07/18 16:46:15 by ljerinec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,12 @@ void	prompt(t_data *big_data)
 	while (1)
 	{
 		input = readline("Minishell >> ");
-		big_data->input = input;
 		if (ft_strncmp(input, "", ft_strlen(input)) != 0)
 		{
 			add_history(input);
-			ft_printf(" minishell: %s: command not found\n", input);
+			big_data->input = input;
+			// parsing(big_data);
+			ft_printf(" minishell: %s: command not found\n", big_data->input);
 		}
 		if (input == NULL)
 		{
