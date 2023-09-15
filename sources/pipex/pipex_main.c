377db1b4a4ce606bc5bfb0b0700dbf402f1d015d
705mmes <muniosamuel@gmail.com>
2023-09-15 18:57:47 +0200

    fixing pipes

--- sources/pipex/pipex_main.c
+++ sources/pipex/pipex_main.c
@@ -6,7 +6,7 @@
 /*   By: sammeuss <sammeuss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 15:12:11 by sammeuss          #+#    #+#             */
-/*   Updated: 2023/09/15 18:53:00 by sammeuss         ###   ########.fr       */
+/*   Updated: 2023/09/15 18:55:14 by sammeuss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,5 +49,5 @@ void	exec(t_data *big_data)
 		create_childs(big_data);
 	else
 		return ;
-	print_lst_parsing(big_data->lst_parsing->first);
+	// print_lst_parsing(big_data->lst_parsing->first);
 }
