311fe17682751ac29eeeb2be60e5497735cb9b71
Samuel Munio <smunio@k1r2p3.42mulhouse.fr>
2023-09-19 14:58:17 +0200

    cd fixed

--- sources/builtins/cd.c
+++ sources/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: smunio <smunio@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 14:48:53 by sammeuss          #+#    #+#             */
-/*   Updated: 2023/09/19 14:43:53 by smunio           ###   ########.fr       */
+/*   Updated: 2023/09/19 14:57:28 by smunio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,53 +14,52 @@
 
 extern int	g_mini_sig;
 
-void	ft_cd(t_content *content, t_data *big_data)
+void	ft_cd(t_content *cont, t_data *big_data)
 {
 	char	*pwd;
 	char	*path;
 
 	pwd = NULL;
 	path = NULL;
-	if (chdir(content->cmd[1]) != -1)
+	if (chdir(cont->cmd[1]) != -1)
 		return ;
-	if (content->cmd[1] != NULL)
-		path = check_tilde(content, big_data, pwd);
+	if (cont->cmd[1] != NULL)
+		path = check_tilde(cont, big_data, pwd, path);
 	else
 		path = big_data->root_path;
 	if (opendir(path) == NULL)
 	{
-		ft_printf("minishell: cd: %s:", content->cmd[1]);
-		perror(" ");
-		content->exit_code = 1;
+		ft_printf("minishell: cd: %s: ", cont->cmd[1]);
+		perror("");
+		cont->exit_code = 1;
 	}
 	else
 		chdir(path);
 }
 
-char	*check_tilde(t_content *content, t_data *big_data, char *pwd)
+char	*check_tilde(t_content *cont, t_data *big_data, char *pwd, char *path)
 {
-	char	*path;
-
-	path = NULL;
-	if (!ft_strncmp("~", content->cmd[1], 1))
+	if (!ft_strncmp("~", cont->cmd[1], 1))
 	{
 		path = ft_strjoin(path, big_data->root_path);
-		content->cmd[1]++;
-		if (!ft_strncmp("/", content->cmd[1], 1))
+		cont->cmd[1]++;
+		if (!ft_strncmp("/", cont->cmd[1], 1))
 		{
-			content->cmd[1]++;
-			if (content->cmd[1][0] != 0)
+			cont->cmd[1]++;
+			if (cont->cmd[1][0] != 0)
 			{
 				path = ft_strjoin(path, ft_strdup("/"));
-				path = ft_strjoin(path, content->cmd[1]);
+				path = ft_strjoin(path, cont->cmd[1]);
 			}
 		}
+		else if (cont->cmd[1][0] != 0)
+			return (NULL);
 	}
 	else
 	{
 		pwd = getcwd(pwd, CWD_SIZE);
 		path = ft_strjoin(pwd, ft_strdup("/"));
-		path = ft_strjoin(path, content->cmd[1]);
+		path = ft_strjoin(path, cont->cmd[1]);
 	}
 	return (path);
 }
